e19e0c4bdfcaea032a27ad0b366974d5f94b5d25
aknoss <pedrorfig27@gmail.com>
2026-03-10 08:32:07 -0300

    add dotenv

--- dist/src/routes/pokemon.d.ts
+++
@@ -1,8 +0,0 @@
-import { Router } from 'express';
-import * as defaultService from '../services/pokeapi';
-interface PokemonService {
-    getAllGen1Pokemon: typeof defaultService.getAllGen1Pokemon;
-    getPokemonDetail: typeof defaultService.getPokemonDetail;
-}
-declare function createRouter(service?: PokemonService): Router;
-export = createRouter;
--- server.ts
+++ server.ts
@@ -1,11 +1,15 @@
+import dotenv from 'dotenv';
 import express from 'express';
 import path from 'path';
 import createPokemonRoutes from './src/routes/pokemon';
 import { getAllGen1Pokemon } from './src/services/pokeapi';
 
+dotenv.config();
+
 const app = express();
 const PORT = process.env.PORT || 3000;
 
+console.log(PORT);
 app.set('view engine', 'ejs');
 app.set('views', path.join(process.cwd(), 'views'));
 app.use(express.static(path.join(process.cwd(), 'public')));
